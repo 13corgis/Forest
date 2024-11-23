5bfd5b08514a74e7159f0b7cf2e9d48f3dba35c5
Gerrybev Enong <egerrybev@gmail.com>
2024-11-24 03:02:37 +0800

    create app window

---
+++ main.py
@@ -0,0 +1,14 @@
+from kivymd.app import MDApp
+
+class ForestApp(MDApp):
+    def __init__(self, **kwargs):
+        super(ForestApp, self).__init__(**kwargs)
+        pass
+    
+    def build(self):
+        pass
+
+    def on_start(self):
+        pass
+
+ForestApp().run()
