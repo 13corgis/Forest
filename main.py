49d180aee3ce3598aaeb40cc138574ad9a2efcca
Gerrybev Enong <egerrybev@gmail.com>
2024-11-25 18:18:43 +0800

    add load mechanism for the kv files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from kivymd.app import MDApp
+from kivy.lang import Builder
 
 class ForestApp(MDApp):
     def __init__(self, **kwargs):
@@ -6,9 +7,17 @@ class ForestApp(MDApp):
         pass
     
     def build(self):
-        pass
+        self.load_kv_files()
+        
+        return Builder.load_file('kv-files/forest.kv')
 
     def on_start(self):
         pass
+    
+    def load_kv_files(self):
+        Builder.load_file('kv-files/startup_screen.kv')
+        Builder.load_file('kv-files/login_screen.kv')
+        Builder.load_file('kv-files/dashboard_screen.kv')
+        Builder.load_file('kv-files/setup_screen.kv')
 
 ForestApp().run()
