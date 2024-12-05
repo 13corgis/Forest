769bf972ec7aa54a5a4a4c99b422059ed116e654
Gerrybev Enong <egerrybev@gmail.com>
2024-12-05 20:04:54 +0800

    change: database user data update increment from 1 to 20, matching the update interval; member variables related to the resource paths to local variables; and spacing in code structure

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from kivy.core.window import Window
-from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.image import AsyncImage
 from kivy.uix.screenmanager import FadeTransition
+from kivymd.app import MDApp
 from kivy.clock import Clock
-from kivymd.uix.menu import MDDropdownMenu
 from kivymd.uix.dialog import *
+from kivymd.uix.menu import MDDropdownMenu
 from kivymd.uix.relativelayout import MDRelativeLayout
-from pomodoro_timer import Timer
+from screeninfo import get_monitors
 from pymongo import MongoClient
 from datetime import datetime
-from screeninfo import get_monitors
+from pomodoro_timer import Timer
 from user_statistics import UserStatistics
 
-# ! Configure Window - - - - - - - - - - - -
+
+# ! Initialize Window - - - - - - - - - - - -
 def init_window():
     # ! Screen Size (NEED TO ADD VALIDATION FOR MULTIPLE SCREENS)
 
@@ -36,8 +37,6 @@ def init_window():
     Window.left = x
     Window.top = y
 
-    # Other
-    # Window.set_title("Forest")
 
 class ForestApp(MDApp):
     def __init__(self, **kwargs):
@@ -47,7 +46,7 @@ class ForestApp(MDApp):
             self._uri = "mongodb+srv://treeInForest:IAmATree@forest.htdun.mongodb.net/?retryWrites=true&w=majority&appName=Forest"
             self._client = MongoClient(self._uri)
             self._database = self._client["test"]
-            self.collection = self._database["users"]
+            self._collection = self._database["users"]
 
             self.date_today = datetime.today().strftime("%Y-%m-%d")
         except Exception as e:
@@ -61,68 +60,58 @@ class ForestApp(MDApp):
 
         # Statistics
         self.user_statistics = None
-        
+
         # Member Variables - Background
-        self.startup_sequence = 'resources/setup_startup.mp4'
-        self.bg_paths = [
-            'resources/setup_0.mp4',
-            'resources/setup_1.mp4',
-            'resources/setup_2.mp4',
-            'resources/setup_3.mp4',
-            'resources/setup_4.mp4',
-            'resources/setup_5.mp4',
-            'resources/setup_6.mp4'
-        ]
         self.selected_bg = 0
-        
+
         # Member Variables - Menu Components
         self.menu = None
         self.settings_dialog = None
         self.help_dialog = None
-    
+
     def build(self):
+        self.init_bg_images()
+        init_window()
         Window.hide()
-        
+
         self.load_kv_files()
-        
-        Clock.schedule_once(lambda dt: self.enter_app(), 8)
-        
+
         return Builder.load_file('kv-files/forest.kv')
 
     def on_start(self):
         # ! Initializations - - - - - - - - - - - -
-        init_window()
-        self.init_bg_images()
         self.init_settings_dialog()
         self.init_help_dialog()
         self.init_profile_menu()
-        
-        Clock.schedule_once(lambda dt: Window.show(), 0)
+
+        Window.show()
+
+        Clock.schedule_once(lambda dt: self.enter_app(), 5)
 
     # ! Update User Session Data per Timer Tick - - - - - - - - - - - -
     def update_user_session_data(self):
 
-        if self.collection.find_one({'user_id': self.current_user, 'sessions.session_id': self.date_today}) is None:
+        if self._collection.find_one({'user_id': self.current_user, 'sessions.session_id': self.date_today}) is None:
             # ! Log today as a session
-            self.collection.update_one({'user_id': self.current_user}, {
+            self._collection.update_one({'user_id': self.current_user}, {
                 '$push': {'sessions': {'session_id': self.date_today, 'total_work_time': 0, 'total_break_time': 0}}})
         else:
             # ! There is an existing session for today
             # ! So update total work time
             if self.timer.timer_period == 'work':
-                self.collection.update_one({'user_id': self.current_user, 'sessions.session_id': self.date_today},
-                                      {'$inc': {'sessions.$.total_work_time': 1}})
+                self._collection.update_one({'user_id': self.current_user, 'sessions.session_id': self.date_today},
+                                            {'$inc': {'sessions.$.total_work_time': 20}})
             # ! Break period
             elif self.timer.timer_period == 'break':
-                self.collection.update_one({'user_id': self.current_user, 'sessions.session_id': self.date_today},
-                                      {'$inc': {'sessions.$.total_break_time': 1}})
-    
+                self._collection.update_one({'user_id': self.current_user, 'sessions.session_id': self.date_today},
+                                            {'$inc': {'sessions.$.total_break_time': 20}})
+
     # ! Login Attempt - - - - - - - - - - - -
     def attempt_login(self):
         login_textfield_input = self.root.get_screen('login').ids.login_textfield.text
         login_passfield_input = self.root.get_screen('login').ids.login_passfield.text
 
-        if self.collection.find_one({'user_id': login_textfield_input, 'password': login_passfield_input}) is not None:
+        if self._collection.find_one({'user_id': login_textfield_input, 'password': login_passfield_input}) is not None:
 
             self.show_success_dialog()
 
@@ -145,7 +134,7 @@ class ForestApp(MDApp):
         signup_passfield_input = self.root.get_screen('login').ids.signup_passfield.text
         signup_cfpassfield_input = self.root.get_screen('login').ids.signup_cfpassfield.text
 
-        if self.collection.find_one({'user_id': signup_textfield_input}) is not None:
+        if self._collection.find_one({'user_id': signup_textfield_input}) is not None:
             MDDialog(MDDialogIcon(icon='account-check'),
                      MDDialogHeadlineText(text='User ID Taken'),
                      MDDialogSupportingText(
@@ -153,7 +142,7 @@ class ForestApp(MDApp):
         else:
             if len(signup_textfield_input) >= 3 and len(signup_passfield_input) >= 3:
                 if signup_passfield_input == signup_cfpassfield_input:
-                    self.collection.insert_one(
+                    self._collection.insert_one(
                         {'user_id': signup_textfield_input, 'password': signup_passfield_input, 'sessions': []})
 
                     self.show_success_dialog()
@@ -173,7 +162,7 @@ class ForestApp(MDApp):
                              MDDialogHeadlineText(text='Invalid Password'),
                              MDDialogSupportingText(
                                  text='Password must be at least 3 characters long.')).open()
-    
+
     # ! Startup Sequence - - - - - - - - - - - -
     def enter_app(self):
         self.root.transition = FadeTransition(duration=1)
@@ -184,12 +173,13 @@ class ForestApp(MDApp):
 
     def to_login(self):
         self.root.current = 'login'
-        
+
     # ! Profile menu > logout - - - - - - - - - - - -
     def logout(self):
         self.timer.default_timer_settings()
 
         self.root.transition = FadeTransition(duration=0.15)
+
         self.root.get_screen('login').ids.login_passfield.text = ''
         self.root.current = 'login'
         self.menu.dismiss()
@@ -206,7 +196,7 @@ class ForestApp(MDApp):
                                   md_bg_color=(0, 0, 0, 0))
         success_dialog.open()
 
-        Clock.schedule_once(lambda dt: success_dialog.dismiss(), 1)
+        Clock.schedule_once(lambda dt: success_dialog.dismiss(), 2.5)
 
     # ! Change Dashboard Opacity on Dialog.open() and Dialog.dismiss() - - - - - - - - - - - -
     def change_opacity(self):
@@ -216,14 +206,25 @@ class ForestApp(MDApp):
         ]
 
         for comp in components:
-            opacity = comp.opacity
-            if opacity == 1 or opacity == 0.6:
+            if comp.opacity == 1 or comp.opacity == 0.6:
                 comp.opacity = 0
-            elif opacity == 0:
-                comp.opacity = 1
-    
+            elif comp.opacity == 0:
+                if comp == components[0]:
+                    comp.opacity = 1
+                else:
+                    comp.opacity = 0.6
+
     # ! Selected Background Tracker - - - - - - - - - - - -
-    def manage_background(self, action):
+    def manage_background(self, action, bg_paths=None):
+
+        if bg_paths is None:
+            bg_paths = ['resources/setup_0.mp4',
+                        'resources/setup_1.mp4',
+                        'resources/setup_2.mp4',
+                        'resources/setup_3.mp4',
+                        'resources/setup_4.mp4',
+                        'resources/setup_5.mp4',
+                        'resources/setup_6.mp4']
 
         if action == 'next':
             if self.selected_bg == 6:
@@ -236,8 +237,8 @@ class ForestApp(MDApp):
             else:
                 self.selected_bg -= 1
 
-        self.root.get_screen('setup').ids.setup_bg.source = self.bg_paths[self.selected_bg]
-        
+        self.root.get_screen('setup').ids.setup_bg.source = bg_paths[self.selected_bg]
+
     # ! Initialize Profile Menu - - - - - - - - - - - -
     def init_profile_menu(self):
         self.menu = MDDropdownMenu(
@@ -276,7 +277,7 @@ class ForestApp(MDApp):
             item['on_release'] = self.menu.dismiss
 
         self.menu.items = menu_items
-    
+
     # ! Initialize Settings Dialog - - - - - - - - - - - -
     def init_settings_dialog(self):
 
@@ -336,14 +337,24 @@ class ForestApp(MDApp):
 
         self.help_dialog.on_open = self.change_opacity
         self.help_dialog.on_dismiss = self.change_opacity
-    
-    # ! Initialize Background Resources - - - - - - - - - - - -
-    def init_bg_images(self):
-        image = AsyncImage(source=self.startup_sequence)
 
-        for path in self.bg_paths:
+    # ! Initialize / Asynchronously Load Background Resources - - - - - - - - - - - -
+    def init_bg_images(self):
+        bg_paths = [
+            'resources/setup_0.mp4',
+            'resources/setup_1.mp4',
+            'resources/setup_2.mp4',
+            'resources/setup_3.mp4',
+            'resources/setup_4.mp4',
+            'resources/setup_5.mp4',
+            'resources/setup_6.mp4'
+        ]
+
+        image = AsyncImage(source='resources/setup_startup.mp4')
+
+        for path in bg_paths:
             image = AsyncImage(source=path)
-    
+
     # ! Load kv files - - - - - - - - - - - -
     def load_kv_files(self):
         Builder.load_file('kv-files/startup_screen.kv')
@@ -355,5 +366,6 @@ class ForestApp(MDApp):
     # ! Get Timer Label - - - - - - - - - - - -
     def get_timer_label(self):
         return self.root.get_screen('dashboard').ids.timer_label
+
 
 ForestApp().run()
